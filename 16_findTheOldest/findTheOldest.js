1ef0d64143cfc56c80864a4b60bd4a938abf9c11
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-16 19:02:11 -0400

    completed exercises 13-16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(person) {
+    // gets current year IF deathYear is nullish
+    const deathYear = person.yearOfDeath ?? new Date().getFullYear();
+    return deathYear - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+  if (!people.length) return null; // if there are no people, return null
+  
+  return people.reduce((oldest, current) => {
+    return getAge(current) > getAge(oldest) ? current : oldest;
+  });
 };
 
 // Do not edit below this line
